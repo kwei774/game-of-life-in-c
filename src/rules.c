a90592c8c403075d4eb0cf8dcea828f5cdb799bb
Kevin <5399649+kwei774@users.noreply.github.com>
2019-12-14 11:04:41 -0500

    rule 3

--- src/rules.c
+++ src/rules.c
@@ -2,7 +2,7 @@
 
 bool cell_alive(bool alive_now, int neighbors)
 {
-    if (neighbors >= 2){
+    if (neighbors == 2 || neighbors == 3){
         return true;
     }
     return false;
--- test/test_rules.c
+++ test/test_rules.c
@@ -10,8 +10,8 @@ TEST_TEAR_DOWN(Rules) {}
 
 TEST(Rules, live_cell_with_fewer_than_two_neighbors_dies)
 {
-    TEST_ASSERT_FALSE(cell_alive(true, 0));
-    TEST_ASSERT_FALSE(cell_alive(true, 1));
+    TEST_ASSERT_FALSE(cell_alive(true, 0))
+    TEST_ASSERT_FALSE(cell_alive(true, 1))
 }
 
 TEST(Rules, live_cell_with_two_or_three_neighbors_lives)
@@ -20,8 +20,15 @@ TEST(Rules, live_cell_with_two_or_three_neighbors_lives)
     TEST_ASSERT_TRUE(cell_alive(true, 3))
 }
 
+TEST(Rules, live_cell_with_four_or_more_neighbors_dies)
+{
+    TEST_ASSERT_FALSE(cell_alive(true, 4))
+    TEST_ASSERT_FALSE(cell_alive(true, 5))
+}
+
 TEST_GROUP_RUNNER(Rules)
 {
-    RUN_TEST_CASE(Rules, live_cell_with_fewer_than_two_neighbors_dies);
-    RUN_TEST_CASE(Rules, live_cell_with_two_or_three_neighbors_lives);
+    RUN_TEST_CASE(Rules, live_cell_with_fewer_than_two_neighbors_dies)
+    RUN_TEST_CASE(Rules, live_cell_with_two_or_three_neighbors_lives)
+    RUN_TEST_CASE(Rules, live_cell_with_four_or_more_neighbors_dies)
 }
